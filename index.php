8d7021eb8bcbee1c8586b5f7faa9b02555300567
tka lu <tka@mail2000.com.tw>
2009-11-30 11:23:11 +0800

    unique string hash

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <!doctype html>
 <?
+mb_internal_encoding( 'UTF-8');
+mb_regex_encoding( 'UTF-8'); 
+
+function mbStringToArray( $string) {
+    $stop   = mb_strlen( $string);
+    $result = array();
+
+    for( $idx = 0; $idx < $stop; $idx++)
+    {
+        $result[] = mb_substr( $string, $idx, 1);
+    }
+
+    return $result;
+} 
+$start=microtime(1);
 if(!empty($_POST['str'])){
     include('font.php');
     $dir='tmp';
-    $includeLatin1=false;
-    $data=file_get_contents('DroidSansFallback.ttf');
-    $font=new Font($data);
-    $str=$_POST['str'];
+    $str=join("",array_unique(mbStringToArray($_POST['str'])));
     $filename=$dir."/".md5($str);
     if(!file_exists($filename.".ttf")){
+        $includeLatin1=false;
+        $data=file_get_contents('DroidSansFallback.ttf');
+        $font=new Font($data);
         $fontData=$font->getSubset($str,$includeLatin1);
         file_put_contents($filename.".ttf",$fontData);
 
@@ -20,23 +35,13 @@ if(!empty($_POST['str'])){
 ?>
 <html>
     <head>
-        <title>test</title>
+        <title>Droid font subset generator</title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <style type="text/css">
-    
-            @font-face {
-                font-family: "Droid subset font-ui";
-                src: url(tmp/droid-subset-ui.eot);
-            }
-            @font-face {
-                font-family: "Droid subset font-ui";
-                src: url(tmp/droid-subset-ui.ttf);
-            }
             textarea,p,input,label{
                 font-size:36px;
             }
             input,label{
-                font-family: "Droid subset font-ui";
                 margin:5px;
             }
             
@@ -65,20 +70,21 @@ if(!empty($_POST['str'])){
         </style>
     </head>
     <body>
-        <label>請輸入需要的字：</label>
+        <label>Char：</label>
         <form method="post">
             <textarea name="str"><?=!empty($_POST['str'])?htmlspecialchars($_POST['str']):''?></textarea>
             <input type="submit">
         </form>
         <?if(!empty($_POST['str'])):?>
             <p>
-            檔案：
+            File:
             <a href="<?=$filename?>.eot">eot</a>
-            <a href="<?=$filename?>.eot">ttf</a>
+            <a href="<?=$filename?>.ttf">ttf</a>
             <p>
             <h2>DEMO:</h2>
             <p class="demo"><?=!empty($_POST['str'])?nl2br(htmlspecialchars($_POST['str'])):''?></p>
         <?endif;?>
+        <?echo "spend time:".(microtime(1)-$start)." sec"?>
     </body>
 </html>
 
